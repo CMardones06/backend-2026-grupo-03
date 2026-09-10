181d0611ce370ad30210e1be71e906fa2b5fc114
maria-hc <mhenriquez2026@alu.uct.cl>
2026-09-10 15:23:11 -0300

    Agrega controladores para especies y riegos completando los 12 endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,19 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PlantasController } from './controllers/plantas.controller';
+import { EspeciesController } from './controllers/especies.controller';
+import { RiegosController } from './controllers/riegos.controller';
 import { PlantasService } from './services/plantas.service';
 import { RiegosService } from './services/riegos.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, PlantasController],
+  controllers: [
+    AppController,
+    PlantasController,
+    EspeciesController,
+    RiegosController,
+  ],
   providers: [AppService, PlantasService, RiegosService],
 })
 export class AppModule {}
