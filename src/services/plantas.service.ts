bf5d6f4ce3d3aa7f16a1abe62724b3a67f7d6960
maria-hc <mhenriquez2026@alu.uct.cl>
2026-09-22 15:45:01 -0300

    fix: optimiza servicio de plantas para lectura limpia de DTOs

--- src/services/plantas.service.ts
+++ src/services/plantas.service.ts
@@ -1,22 +1,20 @@
-import { Injectable, UnprocessableEntityException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class PlantasService {
   private plantas: any[] = [];
-  private especies: any[] = [];
 
   crearPlanta(datos: any) {
-    const especie = this.especies.find((e: any) => e.id === datos.especie_id);
+    const nuevaPlanta = {
+      id: this.plantas.length + 1,
+      ...datos,
+    };
 
-    if (!especie) {
-      throw new UnprocessableEntityException({
-        code: 'ESPECIE_REQUERIDA',
-        message: 'Debe existir la especie para crear la planta',
-      });
-    }
-
-    const nuevaPlanta = { id: this.plantas.length + 1, ...datos };
     this.plantas.push(nuevaPlanta);
     return nuevaPlanta;
+  }
+
+  obtenerTodas() {
+    return this.plantas;
   }
 }
