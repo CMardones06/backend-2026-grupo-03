37f7c5bbf13bde581315f5794625f00f49cb990c
keisy-dayana <epul.keisy@gmail.com>
2026-09-22 21:10:22 -0300

    Refactor: reestructurar parte del directorio del proyecto y eliminar archivos obsoletos

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { HttpErrorFilter } from './http-error.filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // Activar el formateador de errores JSON global
+  app.useGlobalFilters(new HttpErrorFilter())
+
  // Activar validaciones globales usando los DTOs
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true, // Elimina datos basura no definidos en el DTO
-    transform: true,// Transforma strings a números o fechas si el DTO lo pide
-  }));
-// Activar el formateador de errores JSON global
-  app.useGlobalFilters(new HttpErrorFilter());
-  await app.listen(3000);
-
-
-  // Habilita la validación automática de datos de entrada (DTOs)
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist: true,
-      forbidNonWhitelisted: true,
-      transform: true,
-    }),
-  );
+    whitelist: true, // Elimina datos basura no definidos en el DTO
+    forbidNonWhitelisted: true,
+    transform: true,// Transforma strings a números o fechas si el DTO lo pide
+  }),
+);
 
   await app.listen(process.env.PORT ?? 3000);
 }
